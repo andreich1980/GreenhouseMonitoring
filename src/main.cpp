c6972792dd6b93cba8b971970dcf68998889ce5c
Andrew Ponomarev <spargo32@gmail.com>
2023-03-11 10:42:19 +0300

    NTP Client

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 #include "Led.h"
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 WiFiManager wifiManager;
 
 Led led(LED_BUILTIN);
+
+WiFiUDP ntpUDP;
+// Time offset: 3:00 (in seconds)
+// Sync time with server interval: 1 hour (in milliseconds)
+NTPClient timeClient(ntpUDP, "ru.pool.ntp.org", 3 * 60 * 60, 1 * 60 * 60 * 1000);
 
 void setup()
 {
@@ -12,10 +19,14 @@ void setup()
   Serial.println("Starting");
 
   wifiManager.autoConnect("GH-MONITORING", "CUCUMBERS");
+
+  timeClient.begin();
 }
 
 void loop()
 {
-  // Normal mode: short blink once in 5 seconds
-  led.blink(50, 5000);
+  led.blink(50);
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
+  delay(5000);
 }
